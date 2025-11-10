3972975bcb906dc0129d0a8f719560e2d687a2a1
Julian Francis <julian.w.francis@gmail.com>
2025-11-10 10:24:49 +0000

    Moving files from my old lego repo

---
+++ stereo_demo/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'stereo_demo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='julian.w.francis@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'stereo_split_node = stereo_demo.stereo_split_node:main'
+        ],
+    },
+)
